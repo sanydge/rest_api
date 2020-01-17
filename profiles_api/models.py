13a632fcacb7046a91455e1b5481a5bd41dec942
Tirdea Alexandru <sanydge@gmail.com>
2020-01-17 17:43:56 +0200

    creating user database model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermisssionsMixin
 
-# Create your models here.
+
+class UserProfile(AbstractBaseUser, PermisssionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=225, unique=True)
+    name = models.CharField(max_length=225)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    object = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of our user"""
+        return self.email
+
+
+
+
