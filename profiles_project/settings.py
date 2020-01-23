82caeb120d10474bb17a2f415ed667e5f2135ece
sanydge <sanydge@gmail.com>
2020-01-23 18:06:06 +0200

    back to sqlite3

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -75,12 +75,14 @@ WSGI_APPLICATION = 'profiles_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'app_db',
-        'USER': 'app_user',
-        'PASSWORD': 'secret',
-        'HOST': 'mysql',
-        'PORT': 3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'app_db',
+        # 'USER': 'app_user',
+        # 'PASSWORD': 'secret',
+        # 'HOST': 'mysql',
+        # 'PORT': 3306
     }
 }
 
